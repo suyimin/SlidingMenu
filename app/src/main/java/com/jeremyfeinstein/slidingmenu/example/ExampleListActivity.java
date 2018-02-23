d31f89dce98474af54f892e6ad1227594f8a8fda
suyimin <suyimin2001@163.com>
2018-02-23 17:59:37 +0800

    初始提交。

---
+++ app/src/main/java/com/jeremyfeinstein/slidingmenu/example/ExampleListActivity.java
@@ -0,0 +1,14 @@
+package com.jeremyfeinstein.slidingmenu.example;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class ExampleListActivity extends Activity {
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setTitle(R.string.app_name);
+	}
+
+}
